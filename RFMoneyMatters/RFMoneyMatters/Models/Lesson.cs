4e049b3f57d088890435bb71338290ea0e9bf93f
BleonaGerbavci <bg52732@ubt-uni.net>
2025-05-03 12:16:27 +0200

    added Lesson model

---
+++ RFMoneyMatters/RFMoneyMatters/Models/Lesson.cs
@@ -0,0 +1,12 @@
+﻿namespace RFMoneyMatters.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; } // e.g. "Budgeting", "Savings"
+        public string Content { get; set; }
+        public int DifficultyLevel { get; set; }
+
+    }
+}
